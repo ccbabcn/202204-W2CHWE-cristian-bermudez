235dda151a7ad82b5f91e9b80cd4f61d98ef8600
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-15 19:16:31 +0200

    install and configured repository dev tools

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
