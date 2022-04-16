b21363291e9d931c692ca3908fe1a26de55fd861
Cristian Bermudez <ccbabcn@gmail.com>
2022-04-16 09:50:58 +0200

    described initial test suite

--- index.test.js
+++ index.test.js
@@ -1,1 +1,12 @@
+describe("Given invertState function", () => {
+  describe("When it receives the array [false, true, false]", () => {
+    test("Then it should return a new array [true, false, true]", () => {
+      const initialArray = [false, true, false];
+      const expectedArray = [true, false, true];
 
+      const invertInitialArray = invertState(initialArray);
+
+      expect(invertInitialArray).toBe(expectedArray);
+    });
+  });
+});
