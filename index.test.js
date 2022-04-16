d00f8fa5d5e9ceb4b5ccca226168109339da3ca7
Cristian Bermudez <ccbabcn@gmail.com>
2022-04-16 12:19:38 +0200

    implemented countingNeighbours function from it's test suite

--- index.test.js
+++ index.test.js
@@ -19,6 +19,33 @@ describe("Given invertState function", () => {
   });
 });
 
+const countNeighbours = (array) => {
+  const neighboursNumberArray = [];
+  for (let position = 0; position < array.length; position++) {
+    let neighbourCounter = 0;
+    if (position === 0) {
+      if (array[position + 1]) {
+        neighbourCounter++;
+        neighboursNumberArray.push(neighbourCounter);
+      }
+    } else if (position > 0 && position < array.length) {
+      if (array[position - 1]) {
+        neighbourCounter++;
+      }
+      if (array[position + 1]) {
+        neighbourCounter++;
+      }
+      neighboursNumberArray.push(neighbourCounter);
+    } else if (position === array.length) {
+      if (array[position - 1]) {
+        neighbourCounter++;
+        neighboursNumberArray.push(neighbourCounter);
+      }
+    }
+  }
+  return neighboursNumberArray;
+};
+
 describe("Given countNeighbours function", () => {
   describe("When it receives the array [false, true, false]", () => {
     test("Then it should return the array [1, 0, 1]", () => {
